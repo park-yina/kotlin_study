6fc7223925a8e1c51e71a5dda2bccddf1ce34cb0
park-yina <111878820+park-yina@users.noreply.github.com>
2023-08-20 21:03:32 +0900

    +systemservice location

--- app/src/main/java/com/example/location_setting/MainActivity.kt
+++ app/src/main/java/com/example/location_setting/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.location_setting
 
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.location_setting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val manager=getSystemService(LOCATION_SERVICE)as LocationManager
+        var result="ALL Providers:"
+        val providers=manager.allProviders
+        for(i in providers){
+            result+="$i,"
+        }
+        Log.d("test","result")
     }
 }
