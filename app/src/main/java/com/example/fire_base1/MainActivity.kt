6b83e3aab7e347898747cd17f839245325caae00
park-yina <111878820+park-yina@users.noreply.github.com>
2023-08-26 19:58:17 +0900

    파이어베이스로 회원가입 진행하기

--- app/src/main/java/com/example/fire_base1/MainActivity.kt
+++ app/src/main/java/com/example/fire_base1/MainActivity.kt
@@ -2,13 +2,44 @@ package com.example.fire_base1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import com.example.fire_base1.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var auth: FirebaseAuth
+    lateinit var email: String
+    lateinit var password: String
+    lateinit var editText: EditText
+    lateinit var passwordText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding=ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //뷰바인딩을 사용하여 화면을 띄운다.
+        auth = FirebaseAuth.getInstance()
+        editText = findViewById(R.id.input_id)
+        passwordText = findViewById(R.id.input_password)
+        val button = binding.button
+
+        button.setOnClickListener {
+            email = editText.text.toString()
+            password = passwordText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Log.d("testing", "task success")
+                        } else {
+                            Log.d("testing", "fail", task.exception)
+                        }
+                    }
+            } else {
+                Log.d("testing", "empty string")
+            }
+        }
     }
 }
