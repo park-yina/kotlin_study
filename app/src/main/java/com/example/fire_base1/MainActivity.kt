e937614f17996ecdf854b5bcf84735432a514940
park-yina <111878820+park-yina@users.noreply.github.com>
2023-08-27 19:55:53 +0900

    이메일 인증하기+이메일 인증 유저 로그인

--- app/src/main/java/com/example/fire_base1/MainActivity.kt
+++ app/src/main/java/com/example/fire_base1/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.fire_base1
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.Toast
 import com.example.fire_base1.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -14,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var password: String
     lateinit var editText: EditText
     lateinit var passwordText: EditText
+    var imm:InputMethodManager?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.input_id)
         passwordText = findViewById(R.id.input_password)
         val button = binding.button
-
+        imm=getSystemService(android.content.Context.INPUT_METHOD_SERVICE)as InputMethodManager?
         button.setOnClickListener {
             email = editText.text.toString()
             password = passwordText.text.toString()
@@ -32,7 +37,15 @@ class MainActivity : AppCompatActivity() {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            Log.d("testing", "task success")
+                            auth.currentUser?.sendEmailVerification()?.addOnCompleteListener{
+                                sendTask->
+                                if(sendTask.isSuccessful){
+                                    Toast.makeText(this,"인증메일을 발송하였습니다",Toast.LENGTH_LONG).show()
+                                }
+                                else{
+                                    Toast.makeText(this,"인증메일 발송에 실패하였습니다",Toast.LENGTH_LONG).show()
+                                }
+                            }
                         } else {
                             Log.d("testing", "fail", task.exception)
                         }
@@ -41,5 +54,29 @@ class MainActivity : AppCompatActivity() {
                 Log.d("testing", "empty string")
             }
         }
+        binding.button2.setOnClickListener {
+            hidekey()
+            email = editText.text.toString()
+            password = passwordText.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                        auth.currentUser?.let{
+                            Log.d("testing","${auth.currentUser!!.isEmailVerified}")
+                            Log.d("testing","${auth.currentUser!!.email}")
+                            Log.d("testing","${auth.currentUser!!.uid}")
+
+                        }
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+    fun hidekey(){
+       currentFocus?.let { v->
+           imm?.hideSoftInputFromWindow(v.windowToken,0)
+       }
     }
 }
