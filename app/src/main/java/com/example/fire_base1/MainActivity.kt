97766c897efdfb698f744f34bc95b1b6223790d5
park-yina <111878820+park-yina@users.noreply.github.com>
2023-08-29 20:34:09 +0900

    Logout with firebase

--- app/src/main/java/com/example/fire_base1/MainActivity.kt
+++ app/src/main/java/com/example/fire_base1/MainActivity.kt
@@ -8,8 +8,14 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fire_base1.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -22,17 +28,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
         editText = findViewById(R.id.input_id)
         passwordText = findViewById(R.id.input_password)
         val button = binding.button
         imm=getSystemService(android.content.Context.INPUT_METHOD_SERVICE)as InputMethodManager?
+        val requestLauncher=registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+         val task=GoogleSignIn.getSignedInAccountFromIntent(it.data)
+         try{
+             val account=task.getResult(ApiException::class.java)!!
+             val credential=GoogleAuthProvider.getCredential(account.idToken,null)
+             auth.signInWithCredential(credential)
+                 .addOnCompleteListener(this){
+                     task->
+                     if(task.isSuccessful)
+                         Log.d("testing","구글 로그인 성공")
+                     else
+                         Log.d("testing","구글 로그인 실패")
+                 }
+         }catch (e:ApiException){
+             Log.d("testing","예외처리")
+         }
+        }
         button.setOnClickListener {
             email = editText.text.toString()
             password = passwordText.text.toString()
-
             if (email.isNotEmpty() && password.isNotEmpty()) {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this) { task ->
@@ -66,12 +89,28 @@ class MainActivity : AppCompatActivity() {
                             Log.d("testing","${auth.currentUser!!.isEmailVerified}")
                             Log.d("testing","${auth.currentUser!!.email}")
                             Log.d("testing","${auth.currentUser!!.uid}")
-
+                            binding.inputId.isEnabled=false
+                            binding.inputPassword.isEnabled=false
+                            binding.button4.setOnClickListener {
+                                binding.inputId.isEnabled=true
+                                binding.inputPassword.isEnabled=true
+                                auth.signOut()
+                                Toast.makeText(this,"로그아웃 완료",Toast.LENGTH_LONG).show()
+                            }
                         }
                     } else {
                         Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
                     }
                 }
+        }
+        binding.button3.setOnClickListener {
+            hidekey()
+            val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val signInIntent=GoogleSignIn.getClient(this,gso).signInIntent
+            requestLauncher.launch(signInIntent)
         }
     }
     fun hidekey(){
