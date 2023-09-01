4e44e3a80267033723e52121f2e3cd7dd8075063
park-yina <111878820+park-yina@users.noreply.github.com>
2023-09-01 10:04:14 +0900

    customdialog send email address

--- app/src/main/java/com/example/fire_base1/ChangeEmail.kt
+++ app/src/main/java/com/example/fire_base1/ChangeEmail.kt
@@ -4,17 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.example.fire_base1.databinding.ChangeemailBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class ChangeEmail:DialogFragment() {
+    lateinit var auth:FirebaseAuth
+    lateinit var textView: TextView
+    lateinit var binding:ChangeemailBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding=ChangeemailBinding.inflate(inflater,container,false)
+        binding=ChangeemailBinding.inflate(inflater,container,false)
+        auth= FirebaseAuth.getInstance()
         return binding.root
+    }
+
+    override fun onStart() {
+        binding.currentEmail.text="현재 이메일: ${auth.currentUser!!.email}"
+        super.onStart()
     }
 
 }
