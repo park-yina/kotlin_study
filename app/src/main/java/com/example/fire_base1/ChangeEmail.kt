a53a491c5ad4cbef3176b9d04e2ee44cc78ace3b
park-yina <111878820+park-yina@users.noreply.github.com>
2023-09-03 00:04:01 +0900

    이메일 주소 변경을 위해 로그아웃 진행

--- app/src/main/java/com/example/fire_base1/ChangeEmail.kt
+++ app/src/main/java/com/example/fire_base1/ChangeEmail.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.fire_base1.databinding.ChangeemailBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -34,6 +35,18 @@ class ChangeEmail:DialogFragment() {
         if (receivetext != null) {
             binding.currentEmail.text = "현재 이메일: ${auth.currentUser!!.email}"
             binding.changeEmail.text = "변경 예정 이메일:${receivetext}"
+            if(receivetext==binding.currentEmail.text){
+                Toast.makeText(context,"현재 이메일과 수정된 메일의 주소가 같습니다",Toast.LENGTH_LONG).show()
+            }
+            binding.yesBtn.setOnClickListener {
+                dismiss()
+                auth.signOut()
+
+            }
+
+        }
+        if(receivetext==null){
+            Toast.makeText(context,"수정 이메일 값이 전송되지 않았습니다",Toast.LENGTH_SHORT).show()
         }
     }
 }
