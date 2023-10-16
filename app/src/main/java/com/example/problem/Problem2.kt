9820357e5f5af9a30b01abc9fc38e6710906477a
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-16 21:23:43 +0900

    2번 문제 새로 알게된 사항 정리

--- app/src/main/java/com/example/problem/Problem2.kt
+++ app/src/main/java/com/example/problem/Problem2.kt
@@ -2,29 +2,40 @@ package com.example.problem
 
 import org.junit.Test
 import junit.framework.TestCase.assertEquals
+import java.util.Stack
 
 
 class Problem2 {
     @Test
     fun case1() {
         val cryptogram = "browoanoommnaon"
-        val result = Problem2Solve().deleteDuplication(cryptogram)
+        val result = Problem2Solve().checkDuplication(cryptogram)
         assertEquals("brown", result)
     }
 
     @Test
     fun case2() {
         val cryptogram = "zyelleyz"
-        val result = Problem2Solve().deleteDuplication(cryptogram)
+        val result = Problem2Solve().checkDuplication(cryptogram)
         assertEquals("", result)
     }
 
     inner class Problem2Solve {
-        fun deleteDuplication(cryptogram: String): String {
-            var resultString=cryptogram.toSet()
-            if(resultString.isNullOrEmpty())
-                resultString.plus("")
-            return resultString.toString()
+        fun checkDuplication(cryptogram: String):String {
+            var result= Stack<String>()
+            for(index in 0 until cryptogram.length){
+                if(result.empty())
+                    result.push(cryptogram[0].toString())
+                if(result.peek()!=cryptogram[index].toString()) {
+                    result.push(cryptogram[index].toString())
+                }
+                else{
+                    result.pop()
+                }
+            }
+            if(result.empty())
+                return ""
+            return result.joinToString("")
         }
     }
 }
