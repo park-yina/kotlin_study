68cf08af70a24caa14ec0f158d32a4cc6499f8f5
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-17 21:23:09 +0900

    solve:3번 문제

---
+++ app/src/main/java/com/example/problem/Problem3.kt
@@ -0,0 +1,35 @@
+package com.example.problem
+
+import org.junit.Test
+import junit.framework.TestCase.assertEquals
+
+class Problem3 {
+        @Test
+        fun test1(){
+            val number=13
+            val result=4
+            assertEquals(result,SolvePromble3().countClap(number))
+        }
+        @Test
+        fun test2(){
+            val number=33
+            val result=14
+            assertEquals(result,SolvePromble3().countClap(number))
+        }
+    inner class SolvePromble3{
+        fun countClap(number:Int):Int{
+            var result:Int=0
+            var inputNum:Int=number
+            for(clap in 1..inputNum){
+                //0이 아니라 1부터 시작하는 이유는 나누기를 할 때 0이 되지 않게 하기 위해서
+                var nowNumber:Int=clap
+                while(nowNumber!=0) {
+                    if (nowNumber % 10 == 3 || nowNumber % 10 == 6 || nowNumber % 10 == 9)
+                        result++
+                    nowNumber/=10
+                }
+            }
+            return result
+        }
+    }
+}
