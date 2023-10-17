2aa983dcb0d6620d17a7c7ace042c88c07832dea
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-17 21:30:47 +0900

    edit:2번 문제 클래스 중복 오류 수정

--- app/src/main/java/com/example/problem/Problem2.kt
+++ app/src/main/java/com/example/problem/Problem2.kt
@@ -3,27 +3,6 @@ package com.example.problem
 import org.junit.Test
 import junit.framework.TestCase.assertEquals
 import java.util.Stack
-
-class Problem2{
- inner class Problem2Test{
-     @Test
-     fun case1(){
-         val cryptogram="browoanoommnaon"
-         val result=Problem2Solve().deleteDuplication(cryptogram)
-         assertEquals(cryptogram,result)
-     }
-     fun case2(){
-         val cryptogram="zyelleyz"
-         val result=Problem2Solve().deleteDuplication(cryptogram)
-         assertEquals(cryptogram,result)
-     }
- }
-    inner class Problem2Solve{
-        fun deleteDuplication(cryptogram:String):String{
-            var resultString= setOf<String>(cryptogram)
-            if(resultString.isNullOrEmpty())
-                resultString.plus("")
-            return resultString.toString()
 class Problem2 {
     @Test
     fun case1() {
@@ -40,15 +19,14 @@ class Problem2 {
     }
 
     inner class Problem2Solve {
-        fun checkDuplication(cryptogram: String):String {
-            var result=Stack<String>()
-            for(index in 0 until cryptogram.length){
-                if(result.empty())
+        fun checkDuplication(cryptogram: String): String {
+            var result = Stack<String>()
+            for (index in 0 until cryptogram.length) {
+                if (result.empty())
                     result.push(cryptogram[0].toString())
-                if(result.peek()!=cryptogram[index].toString()) {
+                if (result.peek() != cryptogram[index].toString()) {
                     result.push(cryptogram[index].toString())
-                }
-                else{
+                } else {
                     result.pop()
                 }
             }
