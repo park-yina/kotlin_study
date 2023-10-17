e3c3f71fd8cf7217e0de46c9f18402ed184c9c12
park-yina <111878820+park-yina@users.noreply.github.com>
2023-10-17 20:24:51 +0900

    Update Problem2.kt

--- app/src/main/java/com/example/problem/Problem2.kt
+++ app/src/main/java/com/example/problem/Problem2.kt
@@ -2,8 +2,10 @@ package com.example.problem
 
 import org.junit.Test
 import junit.framework.TestCase.assertEquals
+import java.util.Stack
 
 
+<<<<<<< HEAD
 class Problem2{
  inner class Problem2Test{
      @Test
@@ -24,6 +26,37 @@ class Problem2{
             if(resultString.isNullOrEmpty())
                 resultString.plus("")
             return resultString.toString()
+=======
+class Problem2 {
+    @Test
+    fun case1() {
+        var cryptogram = "browoanoommnaon"
+        val result = Problem2Solve().checkDuplication(cryptogram)
+        assertEquals("brown", result)
+    }
+
+    @Test
+    fun case2() {
+        var cryptogram = "zyelleyz"
+        val result = Problem2Solve().checkDuplication(cryptogram)
+        assertEquals("", result)
+    }
+
+    inner class Problem2Solve {
+        fun checkDuplication(cryptogram: String):String {
+            var result=Stack<String>()
+            for(index in 0 until cryptogram.length){
+                if(result.empty())
+                    result.push(cryptogram[0].toString())
+                if(result.peek()!=cryptogram[index].toString()) {
+                    result.push(cryptogram[index].toString())
+                }
+                else{
+                    result.pop()
+                }
+            }
+            return result.joinToString("")
+>>>>>>> 302ab1d (2번 문제 새로 알게된 사항 정리)
         }
     }
 }
